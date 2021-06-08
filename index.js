d83957e0708fff905901cd771ff5dd671ad1f461
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 21:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-/* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord(array=[firstname, familyname, title,  payrateperhour]){
+let personinfo = {
+firstName:array[0],
+familyName:array[1],
+title:array[2],
+payPerHour:array[3],
+timeInEvents:[],
+timeOutEvents:[]
 }
+return personinfo;}
+ function createEmployeeRecords(arrays){
+   let t=[];
+   for (let array in arrays)
+  {
+    t.push(createEmployeeRecord(array))
+  }
+  return t;
+ }
+
+ function createTimeInEvent(date){
+   let date2=date.split(" ");
+   let  type="TimeIn",
+    hour= parseInt(date2[1],10),
+    datee=date2[0]
+this.timeInEvents.push( {
+     type:type,
+     hour: hour,
+     date:datee
+   });
+return this;
+ }
+ function createTimeOutEvent(date){
+   let date2=date.split(" ");
+   let  type="TimeOut",
+    hour= parseInt(date2[1],10),
+    datee=date2[0]
+this.timeOutEvents.push( {
+     type:type,
+     hour: hour,
+     date:datee
+   });
+return this;
+ }
+ function hoursWorkedOnDate(date){
+   let out=this.timeOutEvents.find(person=> return person.date=date);
+   let inn=this.timeInEvents.find(person=> return person.date=date);
+   return (out.hour - inn.hour) /1 00
+ }
