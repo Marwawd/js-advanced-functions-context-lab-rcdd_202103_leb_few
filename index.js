d293949c644adc18882025f7b6b7729c46fb9c07
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-08 21:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,27 +22,29 @@ return personinfo;}
    let  type="TimeIn",
     hour= parseInt(date2[1],10),
     datee=date2[0]
-this.timeInEvents.push( {
+    this.timeInEvents.push( {
      type:type,
      hour: hour,
      date:datee
    });
-return this;
+   return this;
  }
  function createTimeOutEvent(date){
    let date2=date.split(" ");
    let  type="TimeOut",
     hour= parseInt(date2[1],10),
     datee=date2[0]
-this.timeOutEvents.push( {
+    this.timeOutEvents.push( {
      type:type,
      hour: hour,
      date:datee
    });
-return this;
- }
+   return this;
+  }
+
+  
  function hoursWorkedOnDate(date){
    let out=this.timeOutEvents.find(person=> return person.date=date);
    let inn=this.timeInEvents.find(person=> return person.date=date);
-   return (out.hour - inn.hour) /1 00
+   return (out.hour - inn.hour) /100
  }
